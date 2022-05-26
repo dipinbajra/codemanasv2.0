8c14f465dceeb3a55ec4b2d451dd239ede16d320
dipinbajra <bajradipin98@gmail.com>
2022-05-26 13:00:10 +0545

    swiperjs added for both desktop and responsive

--- js/main.js
+++ js/main.js
@@ -2,13 +2,32 @@ function openResponsive(menu) {
     menu.classList.toggle('open');
   }
   
-  const btnScrollToTop = document.getElementById("btnScrollToTop");
   
-  btnScrollToTop.addEventListener("click", function () {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth"
-    });
+const swiperHandler = () =>{
+
+  const swiper = new Swiper('.swiper', {
+    autoplay:{
+      delay: 5000,
+    },
+    effect: "fade",
+    loop: true,
+   
+    autoHeight: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
   
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
   });
+}
+document.addEventListener('DOMContentLoaded', swiperHandler)
