8e4ee429ba315b20857d711c5cd1af5edf1bd964
dipinbajra <bajradipin98@gmail.com>
2022-05-26 18:04:06 +0545

    search popup added

--- js/main.js
+++ js/main.js
@@ -1,33 +1,59 @@
 function openResponsive(menu) {
-    menu.classList.toggle('open');
-  }
-  
-  
-const swiperHandler = () =>{
+  menu.classList.toggle('open');
+}
+
+
+const swiperHandler = () => {
 
   const swiper = new Swiper('.swiper', {
-    autoplay:{
+    autoplay: {
       delay: 5000,
     },
     effect: "fade",
     loop: true,
-   
+
     autoHeight: true,
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
-  
+
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
+
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
   });
-}
-document.addEventListener('DOMContentLoaded', swiperHandler)
+//   const trigger = document.querySelector('#trigger');
+//   const modalWrapper = document.querySelector('.modal-wrapper');
+  // trigger.addEventListener('click', function () { // add an e.prevent here to prevent anchor tag from diverting to links
+//     modalWrapper.classList.add('.active');
+//   })
+ }
+
+
+
+
+document.addEventListener('DOMContentLoaded', swiperHandler);
+
+$(document).ready(function() {
+
+  // $('test-popup').magnificPopup({type:'inline'});
+
+  // $('.test-popup-link').magnificPopup({
+  //   type:'image',
+  //   midClick: true 
+  // });
+
+  $('.open-popup-link').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });
+  
+});
+
