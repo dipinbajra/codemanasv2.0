69919c07df84449ba55d417486c071b014e3c403
dipinbajra <bajradipin98@gmail.com>
2022-05-27 17:38:31 +0545

    pop up responsive, popup on the page reload and pop up image while image is clicked

--- js/main.js
+++ js/main.js
@@ -29,19 +29,30 @@ const swiperHandler = () => {
       el: '.swiper-scrollbar',
     },
   });
-//   const trigger = document.querySelector('#trigger');
-//   const modalWrapper = document.querySelector('.modal-wrapper');
+  //   const trigger = document.querySelector('#trigger');
+  //   const modalWrapper = document.querySelector('.modal-wrapper');
   // trigger.addEventListener('click', function () { // add an e.prevent here to prevent anchor tag from diverting to links
-//     modalWrapper.classList.add('.active');
-//   })
- }
+  //     modalWrapper.classList.add('.active');
+  //   })
+}
 
 
 
 
 document.addEventListener('DOMContentLoaded', swiperHandler);
 
-$(document).ready(function() {
+// onPage load magnific popup
+
+$(window).load(function () {
+  $.magnificPopup.open({
+    items: { src: '#second-popup' }, type: 'inline'
+  }, 0);
+});
+
+//End onPage load magnific popup
+
+
+$(document).ready(function () {
 
   // $('test-popup').magnificPopup({type:'inline'});
 
@@ -51,9 +62,52 @@ $(document).ready(function() {
   // });
 
   $('.open-popup-link').magnificPopup({
-    type:'inline',
+    type: 'inline',
     midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
   });
-  
+
 });
 
+
+ $(document).ready(function() {
+
+  // $('.image-popup').magnificPopup({
+	// 	type: 'image',
+	// 	closeOnContentClick: true,
+	// 	mainClass: 'mfp-img-mobile',
+	// 	image: {
+	// 		verticalFit: true
+	// 	}
+		
+	// });
+
+
+  // $('.image-popup').magnificPopup({
+	// 	type: 'image',
+	// 	closeOnContentClick: true,
+	// 	image: {
+	// 		verticalFit: false
+	// 	}
+	// });
+
+
+$('.image-popup').magnificPopup({
+  type: 'image',
+  closeOnContentClick: true,
+  closeBtnInside: false,
+  fixedContentPos: true,
+  mainClass: ' mfp-with-zoom', // class to remove default margin from left and right side
+  image: {
+    verticalFit: true
+  },
+  zoom: {
+    enabled: true,
+    duration: 600 // don't foget to change the duration also in CSS
+  }
+});
+
+ });
+
+
+
+	
